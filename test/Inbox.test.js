6da93046cd21e5785f843d1bb9876983f4778537
MarkoVoitovych <searchangeos@gmail.com>
2024-05-08 11:41:27 +0300

    deployed

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,31 +1,61 @@
 const ganache = require('ganache');
-const { describe, it } = require('mocha');
-const { Web3 } = require('web3');
+const { describe, it, beforeEach } = require('mocha');
+const { default: Web3 } = require('web3');
 const assert = require('assert');
+// const forge = require('node-forge');
+
+const { interface, bytecode } = require('../compile');
+
+global.crypto = {
+  getRandomValues: function (arr) {
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] = Math.floor(Math.random() * 256);
+    }
+    return arr;
+  },
+};
 
 const web3 = new Web3(ganache.provider());
 
-// class Car {
-// 	park() {
-// 		return 'stopped';
-// 	}
+let accounts;
+let inbox;
+const initialString = 'Hi there!';
 
-// 	drive() {
-// 		return 'vroom';
-// 	}
-// }
-// let car
+beforeEach(async () => {
+  // get a list of all accounts
+  accounts = await web3.eth.getAccounts();
 
-// beforeEach(() => {
-// 	car = new Car();
-// })
+  // use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: [initialString],
+    })
+    .send({
+      from: accounts[0],
+      gas: '1000000',
+    });
 
-// describe('Car', () => {
-// 	it('can park', () => {
-// 		assert.equal(car.park(), 'stopped')
-// 	});
+  // const rsaKeyPair = forge.pki.rsa.generateKeyPair({ bits: 2048 });
+  // const publicKeyPem = forge.pki.publicKeyToPem(rsaKeyPair.publicKey);
+  // const privateKeyPem = forge.pki.privateKeyToPem(rsaKeyPair.privateKey);
+});
 
-// 	it('can drive', () => {
-// 		assert.equal(car.drive(), 'vroom')
-// })
-// })
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, initialString);
+  });
+
+  it('can modify a message', async () => {
+    await inbox.methods.setMessage('Bye there!').send({
+      from: accounts[0],
+    });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Bye there!');
+  });
+});
