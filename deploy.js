76b2243a4c9b7e1fe7c07a8f98b82ec03bd28105
MarkoVoitovych <searchangeos@gmail.com>
2024-05-08 14:42:38 +0300

    finished

--- deploy.js
+++ deploy.js
@@ -1,5 +1,28 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const { default: Web3 } = require('web3');
+require('dotenv').config();
 
-// deploy code will go here
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+   process.env.METAMASK_ACC_MNEMONIC,
+   process.env.SEPOLIA_INFURA_ENDPOINT
+);
+const initialString = 'Hi there!';
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+   const accounts = await web3.eth.getAccounts();
+   const contract = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({
+         data: bytecode,
+         arguments: [initialString],
+      })
+      .send({
+         from: accounts[0],
+         gas: '1000000',
+      });
+   console.log('contract', contract.options.address);
+   provider.engine.stop();
+};
+deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,60 +2,54 @@ const ganache = require('ganache');
 const { describe, it, beforeEach } = require('mocha');
 const { default: Web3 } = require('web3');
 const assert = require('assert');
-// const forge = require('node-forge');
 
 const { interface, bytecode } = require('../compile');
 
 global.crypto = {
-  getRandomValues: function (arr) {
-    for (var i = 0; i < arr.length; i++) {
-      arr[i] = Math.floor(Math.random() * 256);
-    }
-    return arr;
-  },
+   getRandomValues: function (arr) {
+      for (var i = 0; i < arr.length; i++) {
+         arr[i] = Math.floor(Math.random() * 256);
+      }
+      return arr;
+   },
 };
 
 const web3 = new Web3(ganache.provider());
-
+const initialString = 'Hi there!';
 let accounts;
 let inbox;
-const initialString = 'Hi there!';
 
 beforeEach(async () => {
-  // get a list of all accounts
-  accounts = await web3.eth.getAccounts();
+   // get a list of all accounts
+   accounts = await web3.eth.getAccounts();
 
-  // use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-      arguments: [initialString],
-    })
-    .send({
-      from: accounts[0],
-      gas: '1000000',
-    });
-
-  // const rsaKeyPair = forge.pki.rsa.generateKeyPair({ bits: 2048 });
-  // const publicKeyPem = forge.pki.publicKeyToPem(rsaKeyPair.publicKey);
-  // const privateKeyPem = forge.pki.privateKeyToPem(rsaKeyPair.privateKey);
+   // use one of those accounts to deploy the contract
+   inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({
+         data: bytecode,
+         arguments: [initialString],
+      })
+      .send({
+         from: accounts[0],
+         gas: '1000000',
+      });
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {
-    assert.ok(inbox.options.address);
-  });
+   it('deploys a contract', () => {
+      assert.ok(inbox.options.address);
+   });
 
-  it('has a default message', async () => {
-    const message = await inbox.methods.message().call();
-    assert.equal(message, initialString);
-  });
+   it('has a default message', async () => {
+      const message = await inbox.methods.message().call();
+      assert.equal(message, initialString);
+   });
 
-  it('can modify a message', async () => {
-    await inbox.methods.setMessage('Bye there!').send({
-      from: accounts[0],
-    });
-    const message = await inbox.methods.message().call();
-    assert.equal(message, 'Bye there!');
-  });
+   it('can modify a message', async () => {
+      await inbox.methods.setMessage('Bye there!').send({
+         from: accounts[0],
+      });
+      const message = await inbox.methods.message().call();
+      assert.equal(message, 'Bye there!');
+   });
 });
